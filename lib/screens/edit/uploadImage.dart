05c71cc9e07e461c1b4075733f10bdf7e9e2cc8a
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-08-14 20:23:50 +0530

    loading indicator shown as a stack inside the screen while the image loads or other actions are performed

--- lib/screens/edit/uploadImage.dart
+++ lib/screens/edit/uploadImage.dart
@@ -15,14 +15,18 @@ class UploadImage extends StatefulWidget {
 
 class _UploadImageState extends State<UploadImage> {
   File _selectedFile;
+  bool isLoading = false;
 
   Widget getImageWidget(){
     if(_selectedFile!=null){
-      return Image.file(
-        _selectedFile,
-        width: 720,
-        height: 405,
-        fit: BoxFit.cover,
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.file(
+          _selectedFile,
+          width: 405,
+          height: 228,
+          fit: BoxFit.contain
+        ),
       );
     }else{
       return Image.asset('assets/images/bag_1.png');
@@ -30,10 +34,13 @@ class _UploadImageState extends State<UploadImage> {
   }
 
   getimage(ImageSource source)async{
+    this.setState(() {
+      isLoading =true;
+    });
     File image = await ImagePicker.pickImage(source: source); 
     if(image!=null){
       File cropped = await ImageCropper.cropImage(
-        sourcePath: image.path,
+         sourcePath: image.path,
         aspectRatio: CropAspectRatio(ratioX: 16, ratioY: 9),
         compressQuality: 100,
         maxHeight: 1920,
@@ -48,7 +55,14 @@ class _UploadImageState extends State<UploadImage> {
       );
       this.setState(() {
         _selectedFile = cropped;
+        int filesize = _selectedFile.lengthSync();
+        print('${(filesize/1000000).toStringAsFixed(2)} Mb');
+        isLoading = false;
       });
+    }else{
+      this.setState(() {
+      isLoading =false;
+    });
     }
   }
 
@@ -57,126 +71,135 @@ class _UploadImageState extends State<UploadImage> {
     List<String> mylist = ['https://cocooil.com.au/wp-content/uploads/2017/06/TanningOil_New-600x600.jpg','https://cocooil.com.au/wp-content/uploads/2017/06/TanningOil_New-600x600.jpg','https://cocooil.com.au/wp-content/uploads/2017/06/TanningOil_New-600x600.jpg','https://cocooil.com.au/wp-content/uploads/2017/06/TanningOil_New-600x600.jpg'];
     return Scaffold(
           body: SingleChildScrollView(
-              child: Container(
-              height: MediaQuery.of(context).size.height +50,
-        child:Column(
-            children: <Widget>[
-              Headofapp(title: "Bold Alive",subtitle: 'Uplaod Your Custom Image',),
-              
-              SizedBox(height: 10,),
-              // CarouselSlider(
-              //   options: CarouselOptions(
-              //         height: 350,
-              //         aspectRatio: 16/9,
-              //         viewportFraction: 0.8,
-              //         initialPage: 0,
-              //         enableInfiniteScroll: true,
-              //         reverse: false,
-              //         autoPlay: true,
-              //         autoPlayInterval: Duration(seconds: 3),
-              //         autoPlayAnimationDuration: Duration(milliseconds: 800),
-              //         autoPlayCurve: Curves.fastOutSlowIn,
-              //         enlargeCenterPage: true,
-              //         scrollDirection: Axis.horizontal,
-              //     ),
-              //   items: mylist.map((i) {
-              //     return Builder(
-              //       builder: (BuildContext context) {
-              //         return Container(
-              //           width: MediaQuery.of(context).size.width,
-              //           margin: EdgeInsets.symmetric(horizontal: 5.0),
-              //           decoration: BoxDecoration(
-              //             color: Colors.white60
-              //           ),
-              //           child: Image.network(i)
-              //         );
-              //       },
-              //     );
-              //   }).toList(),
-              // ),
-              Container(
-                padding: EdgeInsets.all(15),
-                child: Text(
-                  'data SizedBox(height: 10,), SizedBox(height: 10,),SizedBox(height: 10,),SizedBox(height: 10,),',
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              getImageWidget(),
-              
-              Row(
+              child: Stack(
                 children: <Widget>[
                   Container(
-                      width: MediaQuery.of(context).size.width*0.4,
-                      padding: EdgeInsets.only(top: 3, left: 3),
-                      margin: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: Border(
-                          bottom: BorderSide(color: Colors.black),
-                          top: BorderSide(color: Colors.black),
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-                        )
-                      ),
-                      child: MaterialButton(
-                        minWidth: double.infinity,
-                        height: 50,
-                        onPressed: (){
-                          getimage(ImageSource.camera);
-                        },
-                        color: Colors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        child: Text('Camera', style: TextStyle(
-                          fontWeight: FontWeight.w600, 
-                          fontSize: 18,
-                          color: Colors.black
-                        ),),
-                      ),
-                    ),
-                    Container(
-                  width: MediaQuery.of(context).size.width*0.4,
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  margin: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    border: Border(
-                      bottom: BorderSide(color: Colors.black),
-                      top: BorderSide(color: Colors.black),
-                      left: BorderSide(color: Colors.black),
-                      right: BorderSide(color: Colors.black),
-                    )
-                  ),
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    height: 50,
-                    onPressed: (){
-                      // Navigator.of(context).pushNamed(CompleteOrder.routeName);
-                      getimage(ImageSource.gallery);
-                    },
-                    color: Colors.greenAccent,
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Text('Gallary', style: TextStyle(
-                      fontWeight: FontWeight.w600, 
-                      fontSize: 18,
-                      color: Colors.black
-                    ),),
-                  ),
-                ),
-                ],
-              ),
-                
-                SizedBox(height: 10,),
+                    height: MediaQuery.of(context).size.height +50,
+                      child:Column(
+                          children: <Widget>[
+                            Headofapp(title: "Bold Alive",subtitle: 'Uplaod Your Custom Image',),
+                            
+                            SizedBox(height: 10,),
+                            // CarouselSlider(
+                            //   options: CarouselOptions(
+                            //         height: 350,
+                            //         aspectRatio: 16/9,
+                            //         viewportFraction: 0.8,
+                            //         initialPage: 0,
+                            //         enableInfiniteScroll: true,
+                            //         reverse: false,
+                            //         autoPlay: true,
+                            //         autoPlayInterval: Duration(seconds: 3),
+                            //         autoPlayAnimationDuration: Duration(milliseconds: 800),
+                            //         autoPlayCurve: Curves.fastOutSlowIn,
+                            //         enlargeCenterPage: true,
+                            //         scrollDirection: Axis.horizontal,
+                            //     ),
+                            //   items: mylist.map((i) {
+                            //     return Builder(
+                            //       builder: (BuildContext context) {
+                            //         return Container(
+                            //           width: MediaQuery.of(context).size.width,
+                            //           margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            //           decoration: BoxDecoration(
+                            //             color: Colors.white60
+                            //           ),
+                            //           child: Image.network(i)
+                            //         );
+                            //       },
+                            //     );
+                            //   }).toList(),
+                            // ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              child: Text(
+                                'data SizedBox(height: 10,), SizedBox(height: 10,),SizedBox(height: 10,),SizedBox(height: 10,),',
+                                style: TextStyle(fontSize: 16),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            getImageWidget(),
+                            
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                    width: MediaQuery.of(context).size.width*0.4,
+                                    padding: EdgeInsets.only(top: 3, left: 3),
+                                    margin: EdgeInsets.all(16),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      border: Border(
+                                        bottom: BorderSide(color: Colors.black),
+                                        top: BorderSide(color: Colors.black),
+                                        left: BorderSide(color: Colors.black),
+                                        right: BorderSide(color: Colors.black),
+                                      )
+                                    ),
+                                    child: MaterialButton(
+                                      minWidth: double.infinity,
+                                      height: 50,
+                                      onPressed: (){
+                                        getimage(ImageSource.camera);
+                                      },
+                                      color: Colors.white,
+                                      elevation: 0,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(50)
+                                      ),
+                                      child: Text('Camera', style: TextStyle(
+                                        fontWeight: FontWeight.w600, 
+                                        fontSize: 18,
+                                        color: Colors.black
+                                      ),),
+                                    ),
+                                  ),
+                                  Container(
+                                width: MediaQuery.of(context).size.width*0.4,
+                                padding: EdgeInsets.only(top: 3, left: 3),
+                                margin: EdgeInsets.all(16),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  border: Border(
+                                    bottom: BorderSide(color: Colors.black),
+                                    top: BorderSide(color: Colors.black),
+                                    left: BorderSide(color: Colors.black),
+                                    right: BorderSide(color: Colors.black),
+                                  )
+                                ),
+                                child: MaterialButton(
+                                  minWidth: double.infinity,
+                                  height: 50,
+                                  onPressed: (){
+                                    // Navigator.of(context).pushNamed(CompleteOrder.routeName);
+                                    getimage(ImageSource.gallery);
+                                  },
+                                  color: Colors.greenAccent,
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                  child: Text('Gallary', style: TextStyle(
+                                    fontWeight: FontWeight.w600, 
+                                    fontSize: 18,
+                                    color: Colors.black
+                                  ),),
+                                ),
+                              ),
+                              ],
+                            ),
+                              
+                              SizedBox(height: 10,),
 
-            ],
-        )
-      ),
+                          ],
+                      )
+                    ),
+                    isLoading ? Container(
+                      color: Colors.white,
+                      height: MediaQuery.of(context).size.height *0.95,
+                      child: Center(child: CircularProgressIndicator(backgroundColor: Colors.greenAccent,))
+                    ):Center(),
+                ],
+              )
           ),
     );
   }
